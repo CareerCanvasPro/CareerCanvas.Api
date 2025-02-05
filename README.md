caaa4ee1190f2ac946537b6d7e4ead788e830a9f
Tanvir Ahmed Khan <ftkhanft@gmail.com>
2025-02-06 00:58:59 +0600

    Added deployment script and updated the readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - Install dependencies
 
-```
+```shell
 cd <project_name>
 npm install -g lerna
 npm install -g nodemon
@@ -10,6 +10,25 @@ npm install
 lerna run tsc
 ```
 
+## Deploy in the test environment
+
+- Make sure the git repository is accessible to the server
+- Create a `.env` file in the service directory.
+- Install PM2 globally
+- Run the following command to deploy the project with pm2
+
+```shell
+bash deploy.sh
 ```
+
+## Run the project in development mode
+
+```shell
 lerna run dev
 ```
+
+## Adding new services to `deploy.sh`
+
+- Add proper `ecosystem.config.js` file to the service directory.
+- Make sure the service name is unique and set the `name` property in the `ecosystem.config.js` file.
+- Add the service name to the `ecosystem_files` array in the `deploy.sh` file.
---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Colors for console outputs
+ERROR='\033[0;31m'
+WARNING='\033[1;33m'
+SUCCESS='\033[0;32m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Step 1: Pull the latest code from the repository
+echo -e "${PURPLE}[Deploy.sh]:${NC} Pulling latest code..."
+git pull origin $(git rev-parse --abbrev-ref HEAD)
+
+# Step 2: Build the project using Lerna
+echo -e "${PURPLE}[Deploy.sh]:${NC} Building project..."
+npx lerna run tsc
+
+# Step 3: Deploy ecosystem files using PM2
+ecosystem_files=(
+    "./services/auth/ecosystem.config.js"
+    "./services/media/ecosystem.config.js"
+    "./services/user-management/ecosystem.config.js"
+)
+
+echo -e "${PURPLE}[Deploy.sh]:${NC} Deploying services..."
+for file in "${ecosystem_files[@]}"; do
+    app_name=$(node -pe "require('$file').apps[0].name")
+    if pm2 list | grep -q "$app_name"; then
+        echo -e "${PURPLE}[Deploy.sh]:${NC} Restarting $app_name..."
+        pm2 restart "$file"
+    else
+        echo -e "${PURPLE}[Deploy.sh]:${NC} Starting $app_name..."
+        pm2 start "$file"
+    fi
+done
+
+echo -e "${PURPLE}[Deploy.sh]:${NC} Saving current pm2 process list..."
+pm2 save
+
+# Display PM2 process list
+echo -e "${PURPLE}[Deploy.sh]:${NC} Deployment complete! Current PM2 processes:"
+pm2 list
