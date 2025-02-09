55862fc0354be04e4bfbd1a5ef9a49e495a94020
Md. Mahamudur Rahman <opensohag@gmail.com>
2025-02-09 19:55:36 +0600

    feat: add port and env config to personality test service

--- services/personality-test/ecosystem.config.js
+++ services/personality-test/ecosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
   apps: [
     {
-      args: "run server",
-      // Number of instances (use 'max' for all CPU cores)
-      autorestart: true,
-      exec_mode: "fork",
-      // Use 'cluster' if you want multiple instances
-      instances: 1,
-      // Set to true if you want PM2 to watch for changes
-      max_memory_restart: "500M", // Restart if memory usage exceeds 500MB
       name: "Personality Test Service",
       script: "npm",
+      args: "run server",
+      instances: 1,
+      exec_mode: "fork",
       watch: false,
+      autorestart: true,
+      max_memory_restart: "500M",
+      env: {
+        PORT: 8003,
+        NODE_ENV: "production"
+      }
     },
   ],
 };
