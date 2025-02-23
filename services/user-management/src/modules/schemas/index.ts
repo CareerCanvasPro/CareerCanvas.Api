30819e887b28a7ad30889db0a21b3b4668786354
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-02-23 16:22:53 +0600

    update create profile schema

--- services/user-management/src/modules/schemas/index.ts
+++ services/user-management/src/modules/schemas/index.ts
@@ -4,45 +4,8 @@ export const createProfileSchema = joi
   .object()
   .keys({
     address: joi.string().trim().allow(null),
-    dateOfBirth: joi.number().allow(null),
-    education: joi
-      .array()
-      .items(
-        joi.object().keys({
-          achievements: joi.string().trim().allow(null),
-          certificate: joi
-            .object()
-            .keys({
-              name: joi.string(),
-              size: joi.number(),
-              type: joi.string(),
-              uploadedAt: joi.number(),
-              url: joi.string().uri({
-                scheme: ["https"],
-              }),
-            })
-            .allow(null),
-          field: joi.string().trim().allow(null),
-          graduationDate: joi.number().allow(null),
-          institute: joi.string().trim().allow(null),
-          isCurrent: joi.boolean().default(false),
-        })
-      )
-      .allow(null),
     email: joi.string().email().trim().allow(null),
     name: joi.string().trim().allow(null),
-    occupation: joi
-      .array()
-      .items(
-        joi.object().keys({
-          designation: joi.string().trim().allow(null),
-          from: joi.number().allow(null),
-          isCurrent: joi.boolean().default(false),
-          organization: joi.string().trim().allow(null),
-          to: joi.number().allow(null),
-        })
-      )
-      .allow(null),
     phone: joi
       .string()
       .regex(/^\+[1-9]\d{1,14}$/)
@@ -53,9 +16,8 @@ export const createProfileSchema = joi
         scheme: ["https"],
       })
       .allow(null),
-    skills: joi.array().items(joi.string()).allow(null),
-    userID: joi.string(),
-    username: joi.string(),
+    userID: joi.string().required(),
+    username: joi.string().required(),
   })
   .unknown(false);
 
