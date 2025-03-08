67b47d84dddefb960b05748923f860f7214aa733
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-03-08 20:47:47 +0600

    create createOtp method in auth service file

--- services/auth/src/modules/services/otp.ts
+++ services/auth/src/modules/services/otp.ts
@@ -1,57 +1,20 @@
-import {
-  DynamoDBClient,
-  PutItemCommand,
-  ScanCommand,
-} from "@aws-sdk/client-dynamodb";
-import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
-import { v4 as uuidv4 } from "uuid";
+import { prismaClient } from "../../config";
 
-import { config } from "../../config";
-
-interface IAttribute {
-  name: string;
-  value: unknown;
-}
-
-interface PutOtpParams {
-  otp: string;
-  username: string;
-}
-
-interface ScanOtpsParams {
-  attribute: IAttribute;
-}
-
-export class OtpsDB {
-  private readonly dynamoDBClient = new DynamoDBClient({
-    region: config.aws.region,
-  });
-
-  private readonly keyName = "userID";
-
-  private readonly tableName = "OTP";
-
-  public putOtp = async ({
+export class OtpsDb {
+  public createOtp = async ({
     otp,
     username,
-  }: PutOtpParams): Promise<{ httpStatusCode: number }> => {
-    const user = {
-      expiresAt: Math.floor(Date.now() / 1000) + 900,
-      otp,
-      userID: uuidv4(),
-      username,
-    };
-
-    const {
-      $metadata: { httpStatusCode },
-    } = await this.dynamoDBClient.send(
-      new PutItemCommand({
-        Item: marshall(user),
-        TableName: this.tableName,
-      })
-    );
-
-    return { httpStatusCode };
+  }: {
+    otp: string;
+    username: string;
+  }): Promise<void> => {
+    await prismaClient.otp.create({
+      data: {
+        expiresAt: new Date(Date.now() + 15 * 60 * 1000),
+        otp,
+        username,
+      },
+    });
   };
 
   public scanOtps = async ({
