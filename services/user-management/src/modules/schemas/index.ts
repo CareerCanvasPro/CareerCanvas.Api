fefd0bc8c4d60e81e9e1e9953fbc3f4be55a5931
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-03-12 13:01:28 +0600

    modify update user schema to add goals

--- services/user-management/src/modules/schemas/index.ts
+++ services/user-management/src/modules/schemas/index.ts
@@ -62,6 +62,50 @@ export const updateProfileSchema = joi
       )
       .allow(null),
     fcmToken: joi.string().allow(null),
+    goals: joi
+      .array()
+      .items(
+        joi
+          .string()
+          .valid(
+            "Delegate More Effectively",
+            "Become a Better Listener",
+            "Organize Team-Building Activities",
+            "Provide Constructive Feedback Regularly",
+            "Improve Team Communication",
+            "Take a Non-Tech Course",
+            "Travel to a New Place",
+            "Learn a New Language",
+            "Try a New Hobby",
+            "Start a Journaling Habit",
+            "Develop a Morning Routine",
+            "Build a Strong Professional Network",
+            "Enhance Public Speaking Skills",
+            "Read a Business or Self-Development Book",
+            "Improve Time Management",
+            "Develop a Side Project",
+            "Master a Project Management Tool",
+            "Speak at a Tech Conference",
+            "Write Technical Blog Posts",
+            "Mentor a Junior Developer",
+            "Implement Zero Trust Security",
+            "Set Up a Honeypot",
+            "Get a Cybersecurity Certification",
+            "Perform a Security Audit",
+            "Improve Security Practices",
+            "Build a Personal Data Dashboard",
+            "Implement an AI Chatbot",
+            "Work with Big Data",
+            "Learn SQL and NoSQL Databases",
+            "Train and Deploy a Machine Learning Model",
+            "Automate a Repetitive Task",
+            "Contribute to Open Source",
+            "Improve Code Quality",
+            "Master a New Programming Language",
+            "Build and Deploy a Full-Stack App"
+          )
+      )
+      .allow(null),
     interests: joi.array().items(joi.string()).allow(null),
     isEducationDeleted: joi.boolean(),
     isOccupationDeleted: joi.boolean(),
