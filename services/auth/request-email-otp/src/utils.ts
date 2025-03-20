91db47c370bc35b23beffd850fa3ac03dfa828e3
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-03-20 19:56:16 +0600

    delete unnecessary files

--- services/auth/request-email-otp/src/utils.ts
+++
@@ -1,2 +0,0 @@
-export const cleanMessage = (message: string): string =>
-  message.split('"').join("");
--- services/auth/request-email-otp/src/validators.ts
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-export const emailValidator = joi
-  .object()
-  .keys({
-    email: joi.string().email().required().trim(),
-  })
-  .unknown(false);
